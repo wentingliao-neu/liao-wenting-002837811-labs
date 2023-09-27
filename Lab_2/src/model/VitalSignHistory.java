8f193d3ff0646a589ebc458ad87815c57b0bad34
wentingliao-neu <liao.went@northeastern.edu>
2023-09-26 23:11:59 -0400

    lab 2 by wenting liao

---
+++ Lab_2/src/model/VitalSignHistory.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+import java.util.ArrayList;
+/**
+ *
+ * @author lwt
+ */
+public class VitalSignHistory {
+    private ArrayList<VitalSigns>history;
+    
+    public VitalSignHistory(){
+        this.history=new ArrayList<VitalSigns>();
+    }
+
+    public ArrayList<VitalSigns> getHistory() {
+        return history;
+    }
+
+    public void setHistory(ArrayList<VitalSigns> history) {
+        this.history = history;
+    }
+    public VitalSigns addNewVital(){
+        VitalSigns newVitals=new VitalSigns();
+        history.add(newVitals);
+        return newVitals;
+    }
+
+    public void deleteVitals(VitalSigns vs) {
+        history.remove(vs); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
